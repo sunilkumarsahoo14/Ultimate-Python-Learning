1a8faf7608006e1f34a4bd29eca2b5d1aecef76c
Sunil Kumar Sahoo <101491022+sunilkumarsahoo14@users.noreply.github.com>
2024-07-24 15:25:51 +0530

    Perfect Guess In Python

---
+++ Project 2 - The Perfect Guess/main.py
@@ -0,0 +1,24 @@
+"""
+We are going to write a program that generates a random number and asks the user to 
+guess it.
+If the player’s guess is higher than the actual number, the program displays “Lower 
+number please”. Similarly, if the user’s guess is too low, the program prints “higher 
+number please” When the user guesses the correct number, the program displays the 
+number of guesses the player used to arrive at the number.
+Hint: Use the random module.
+"""
+
+import random
+n = random.randint(1, 100)
+a = -1
+guesses = 1
+while(a != n):
+    a = int(input("Guess The Number: "))
+    if(a>n):
+        print("Lower Number Please!")
+        guesses +=1
+    elif(a<n):
+        print("Higher Number Please!")
+        guesses +=1
+
+print(f"You have guess the number {n} correctly in {guesses} guesses.")
